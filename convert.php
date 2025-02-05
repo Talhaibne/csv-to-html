1ce139d53fd8e4fb1f4886acdf8b274df41c5a1a
Md Talha Ibne Anwar <104475054+Talhaibne@users.noreply.github.com>
2025-02-05 01:43:07 -0800

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,1 +1,22 @@
+<?php
+$filename = "Titanic-Dataset.csv";
+if (($handle = fopen($filename, "r")) !== FALSE) {
 
+  echo "<table border='1'>";
+
+  $rowNumber = 0;
+  while (($data = fgetcsv($handle, 1000, ",")) !==FALSE) {
+    if ($rowNumber == 0) {
+      echo "<thead><tr>";
+      foreach ($data as $header) {
+        echo "<th>" . htmlspecialchars($header) . "</th>";
+      }
+      echo "</tr></head><tbody>";
+    } else {
+      echo "<tr>";
+      foreach ($data as $column) {
+        echo "<td>" . htmlspecialchars($column) . "</th>";
+      }
+      echo "</tr>;
+      }
+     
